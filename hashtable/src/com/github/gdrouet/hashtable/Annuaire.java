e84e92a5179db15c1a9d12695917c9c0b67870e2
gdrouet <guidrouet@gmail.com>
2014-11-15 11:19:44 +0100

    Add/modify/search entries in menu.

--- hashtable/src/com/github/gdrouet/hashtable/Annuaire.java
+++ hashtable/src/com/github/gdrouet/hashtable/Annuaire.java
@@ -28,6 +28,7 @@ package com.github.gdrouet.hashtable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.function.Function;
 
 /**
  * Un annuaire d'étudiants qui associe chaque nom à un ensemble de propriétés qui les caractérise.
@@ -45,6 +46,43 @@ public enum  Annuaire {
     private Hashtable table = new Hashtable();
 
     /**
+     * Ajoute un étudiant dans la table de hachage.
+     *
+     * @param input fonction de lecture sur l'entrée standard
+     */
+    public void ajouter(final Function<String, String> input) {
+        final Etudiant e = new Etudiant(input.apply("Nom: "), input.apply("Prénom: "), input.apply("Année: "));
+        table.add(e.getLogin(), e);
+        input.apply("Etudiant ajouté. Appuyer sur entrer pour continuer...");
+    }
+
+    /**
+     * Modifie un étudiant dans la table de hachage.
+     *
+     * @param input fonction de lecture sur l'entrée standard
+     */
+    public void modifier(final Function<String, String> input) {
+        final Etudiant e = Etudiant.class.cast(table.get(input.apply("Login: ")));
+
+        if (e == null) {
+            input.apply("Aucun étudiant dans l'annuaire avec ce login. Appuyer sur entrer pour continuer...");
+        } else {
+            final Object old = table.add(e.getLogin(), new Etudiant(e.getNom(), e.getPrenom(), input.apply("Nouvelle année: ")));
+            input.apply("Le nouvel état vient en remplacement de: " + old + ". Appuyer sur entrer pour continuer...");
+        }
+    }
+
+    /**
+     * Recherche un étudiant et affiche le résultat.
+     *
+     * @param input fonction de lecture sur l'entrée standard
+     */
+    public void rechercher(final Function<String, String> input) {
+        final Etudiant e = Etudiant.class.cast(table.get(input.apply("Login: ")));
+        input.apply((e == null ? "Aucun étudiant dans l'annuaire avec ce login" : e) + "\nAppuyer sur entrer pour continuer...");
+    }
+
+    /**
      * <p>
      * Main method.
      * </p>
@@ -53,7 +91,18 @@ public enum  Annuaire {
      * @throws IOException if any I/O error occurs
      */
     public static void main(final String[] args) throws IOException {
+
+        // Lecture sur l'entrée standard
         final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        final Function<String, String> input = (lbl) -> {
+            try {
+                System.out.print(lbl);
+                return reader.readLine();
+            } catch (IOException ioe) {
+                ioe.printStackTrace(System.out);
+                return null;
+            }
+        };
 
         // Boucle tant qu'on ne sort pas
         menu: for (;;) {
@@ -65,22 +114,21 @@ public enum  Annuaire {
             System.out.println("5. Sortir");
 
             // Lire le choix
-            System.out.print("Choix :");
-            final String choice = reader.readLine();
+            final String choice = input.apply("Choix: ");
 
             // Evaluer the choice
             switch (choice) {
                 case "1":
-                    // TODO
+                    INSTANCE.ajouter(input);
                     break;
                 case "2":
-                    // TODO
+                    INSTANCE.modifier(input);
                     break;
                 case "3":
-                    // TODO
+                    new UnsupportedOperationException().printStackTrace(System.out);
                     break;
                 case "4":
-                    // TODO
+                    INSTANCE.rechercher(input);
                     break;
                 case "5":
                     System.out.println("Bye.");
---
+++ hashtable/src/com/github/gdrouet/hashtable/Etudiant.java
@@ -0,0 +1,109 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Guillaume DROUET
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+package com.github.gdrouet.hashtable;
+
+/**
+ * Un étudiant enregistré en annuaire.
+ */
+public class Etudiant {
+
+    /**
+     * Nom.
+     */
+    private String nom;
+
+    /**
+     * Prénom.
+     */
+    private String prenom;
+
+    /**
+     * Année d'étude.
+     */
+    private String annee;
+
+    /**
+     * Construit une nouvelle instance.
+     *
+     * @param nom le nom
+     * @param prenom le prénom
+     * @param annee l'année
+     */
+    public Etudiant(final String nom, final String prenom, final String annee) {
+        this.nom = nom;
+        this.annee = annee;
+        this.prenom = prenom;
+    }
+
+    /**
+     * Récupère le nom.
+     *
+     * @return le nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Récupère le prénom.
+     *
+     * @return le prénom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * Récupère l'année
+     *
+     * @return l'année
+     */
+    public String getAnnee() {
+        return annee;
+    }
+
+    /**
+     * Récupère le nom.
+     *
+     * @return le nom
+     */
+    public String getLogin() {
+        return (getPrenom().charAt(0) + getNom()).toLowerCase();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "Etudiant {" +
+                "login='" + getLogin() +
+                "'nom='" + getNom() +
+                "', prenom='" + getPrenom() +
+                "', annee='" + getAnnee() +
+                "'}";
+    }
+}
